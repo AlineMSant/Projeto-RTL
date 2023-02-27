c062c9f0aea5647aa4d9ec32869bba19a1248907
AlineMSant <alinems4120@gmail.com>
2023-02-27 15:17:13 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+const { screen } = require('@testing-library/react');
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  beforeEach(() => renderWithRouter(<App />));
+
+  test('O primeiro link deve possuir o texto Home', () => {
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeVisible();
+  });
+
+  test('O segundo link deve possuir o texto About', () => {
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeVisible();
+  });
+
+  test('O terceiro link deve possuir o texto Favorite Pokémon', () => {
+    const linkFavoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    expect(linkFavoritePokemon).toBeVisible();
+  });
+});
+
+// test('', () => {});
