839610cf5894bc2db66db3b5397c96b23696f5a6
AlineMSant <alinems4120@gmail.com>
2023-02-27 16:34:51 -0300

    requisito 3 - refatorado

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -9,35 +9,29 @@ describe('Teste ao favoritar a partir da página de detalhes', () => {
 
   test('É exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
     const linkDetails = screen.getByRole('link', { name: 'More details' });
-    expect(linkDetails).toBeVisible();
 
     userEvent.click(linkDetails);
 
     const linkFavoritePokemon = screen.getByRole('link', { name: favoriteTitleText });
-    expect(linkFavoritePokemon).toBeVisible();
 
     userEvent.click(linkFavoritePokemon);
+
     const noFavoriteFoundText = screen.getByText('No favorite Pokémon found');
     expect(noFavoriteFoundText).toBeVisible();
   });
 
   test('Teste se apenas são exibidos os Pokémon favoritados', () => {
     const linkDetails = screen.getByRole('link', { name: 'More details' });
-    expect(linkDetails).toBeVisible();
 
     userEvent.click(linkDetails);
 
     const labelFavoritePokemon = screen.getByLabelText('Pokémon favoritado?');
-    expect(labelFavoritePokemon).toBeInTheDocument();
 
     userEvent.click(labelFavoritePokemon);
 
     const linkFavoritePokemon = screen.getByRole('link', { name: favoriteTitleText });
-    expect(linkFavoritePokemon).toBeVisible();
 
     userEvent.click(linkFavoritePokemon);
-    const favoriteTitle = screen.getByRole('heading', { name: favoriteTitleText, level: 2 });
-    expect(favoriteTitle).toBeVisible();
 
     const imgFavorite = screen.getAllByRole('img');
     expect(imgFavorite[0]).toBeVisible();
